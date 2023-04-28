f9b19ea9a8c7a4e37fb88c82ac9a70c192b5c25c
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-28 13:52:54 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	public static void clearScreen() {
+		System.out.println("\003[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		//Lembre-se que as posições de um xadrez são letra e numero (a1)
 		try {
