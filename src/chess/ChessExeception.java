ee56eade3cbf2b81539a6c04c195cb6afe9f0b7c
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-28 13:45:49 -0300

    Moving pieces

--- src/chess/ChessExeception.java
+++ src/chess/ChessExeception.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessExeception extends RuntimeException{
+import boardgame.BoardException;
+
+public class ChessExeception extends BoardException{
 	private static final long serialVersionUID = 1L;
 	
 	public ChessExeception(String message) {
