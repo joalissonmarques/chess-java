c03f0c09ecab3549b3b3dece1fc1b011c41787a6
joalissonmarques <joalisonmarques@hotmail.com>
2023-05-15 21:50:17 -0300

    queen

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,105 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece{
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position posiAux = new Position(0, 0);
+		//Verificar acima (above)
+		posiAux.setValue(position.getRow() - 1, position.getColumn());
+		//Enquanto a posição existir e estiver vaga, ta true
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			//Sendo assim, a peça pode se mover para lá
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setRow(posiAux.getRow() - 1);
+		}
+		//Se a posição ainda for true e possui no caminho uma peça inimiga
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//Verificar a esquerda (left)
+		posiAux.setValue(position.getRow(), position.getColumn() - 1);
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setColumn(posiAux.getColumn() - 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//Verificar a direita (right)
+		posiAux.setValue(position.getRow(), position.getColumn() + 1);
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setColumn(posiAux.getColumn() + 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//Verificar para baixo (below)
+		posiAux.setValue(position.getRow() + 1, position.getColumn());
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setRow(posiAux.getRow() + 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		
+		posiAux.setValue(position.getRow() - 1, position.getColumn() - 1);
+		//Enquanto a posição existir e estiver vaga, ta true
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			//Sendo assim, a peça pode se mover para lá
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setValue(posiAux.getRow() - 1, posiAux.getColumn() - 1);
+		}
+		
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//NORDESTE NE 
+		posiAux.setValue(position.getRow() - 1, position.getColumn() + 1);
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setValue(posiAux.getRow() - 1, posiAux.getColumn() + 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//SUDESTE SE
+		posiAux.setValue(position.getRow() + 1, position.getColumn() + 1);
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setValue(posiAux.getRow() + 1, posiAux.getColumn() + 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		//SUDOESTE SW
+		posiAux.setValue(position.getRow() + 1, position.getColumn() - 1);
+		while(getBoard().positionExists(posiAux) && !getBoard().thereIsAPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+			posiAux.setValue(posiAux.getRow() + 1, posiAux.getColumn() - 1);
+		}
+		if(getBoard().positionExists(posiAux) && isThereOpponentPiece(posiAux)) {
+			mat[posiAux.getRow()][posiAux.getColumn()] = true;
+		}
+		
+		return mat;
+	}
+}
